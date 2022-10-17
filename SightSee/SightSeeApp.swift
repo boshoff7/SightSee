fc8f38c2db579c436b7b5f3e86b793309b8c5624
boshoff7 <boshoff7@gmail.com>
2022-10-17 09:53:51 +0200

    Initial Commit

---
+++ SightSee/SightSeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SightSeeApp.swift
+//  SightSee
+//
+//  Created by Chris Boshoff on 2022/10/17.
+//
+
+import SwiftUI
+
+@main
+struct SightSeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
