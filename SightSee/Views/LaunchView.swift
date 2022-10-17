63822d626b24d84f0c5edf162181c88dae2b40a5
boshoff7 <boshoff7@gmail.com>
2022-10-17 09:59:57 +0200

    Initial Commit - Folders for Views, View Model and Models - Remane ContentView to LaunchView

--- SightSee/Views/LaunchView.swift
+++ SightSee/Views/LaunchView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  LaunchView.swift
 //  SightSee
 //
 //  Created by Chris Boshoff on 2022/10/17.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LaunchView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -19,8 +19,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct LaunchView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LaunchView()
     }
 }
