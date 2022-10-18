c7a03153053357d6ff9108e6311e161597b70b7a
boshoff7 <boshoff7@gmail.com>
2022-10-18 13:36:48 +0200

    - Handle tap on the annotation view and show business detail

--- SightSee/Helpers/Constants.swift
+++ SightSee/Helpers/Constants.swift
@@ -9,8 +9,9 @@ import Foundation
 
 struct Constants {
     
-    static var apiKey = "Bearer fttc-jA2quwvkVMgwVV4H508pBzFOC_IU_mHolaAUhVcm8EIaDT2EFAqRqVC003rc0tg0ARRw5UgoabDPSefF7MhIGVajCNXpyGf3IAcdF0DemA6rpd_anmWuzBNY3Yx"
-    static var apiURL = "https://api.yelp.com/v3/businesses/search"
-    static var restuarantsKey = "restuarants"
-    static var sightsKey = "arts"
+    static var apiKey               = "Bearer fttc-jA2quwvkVMgwVV4H508pBzFOC_IU_mHolaAUhVcm8EIaDT2EFAqRqVC003rc0tg0ARRw5UgoabDPSefF7MhIGVajCNXpyGf3IAcdF0DemA6rpd_anmWuzBNY3Yx"
+    static var apiURL               = "https://api.yelp.com/v3/businesses/search"
+    static var restuarantsKey       = "restuarants"
+    static var sightsKey            = "arts"
+    static var annotationReuseId    = "business"
 }
--- SightSee/Views/Home/Map/BusinessMap.swift
+++ SightSee/Views/Home/Map/BusinessMap.swift
@@ -11,6 +11,7 @@ import MapKit
 struct BusinessMap: UIViewRepresentable {
     
     @EnvironmentObject var model: ContentModel
+    @Binding var selectedBusiness: Business?
     
     var locations: [MKPointAnnotation] {
         
@@ -36,6 +37,7 @@ struct BusinessMap: UIViewRepresentable {
     func makeUIView(context: Context) -> MKMapView {
         
         let mapView = MKMapView()
+        mapView.delegate = context.coordinator
         
         // Make the user show up on the map
         mapView.showsUserLocation = true
@@ -57,5 +59,55 @@ struct BusinessMap: UIViewRepresentable {
     static func dismantleUIView(_ uiView: MKMapView, coordinator: ()) {
         
         uiView.removeAnnotations(uiView.annotations)
+    }
+    
+    // MARK: - Coordinator Class
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(map: self)
+    }
+    
+    class Coordinator: NSObject, MKMapViewDelegate {
+        
+        var map: BusinessMap
+        
+        init(map: BusinessMap) {
+            self.map = map
+        }
+        
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            
+            // If the annotation is the user blue dot, return nil
+            if annotation is MKUserLocation { return nil }
+            
+            // Check if there is a reusable annotation view first
+            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: Constants.annotationReuseId)
+            
+            if annotationView == nil {
+                // Create an annotation view
+                annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: Constants.annotationReuseId)
+                annotationView!.canShowCallout = true
+                annotationView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            } else {
+                // We have a reusable one
+                annotationView!.annotation = annotation
+            }
+            
+            // Return it
+            return annotationView
+        }
+        
+        func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+            // User tapped on the annotation
+            
+            // Get the business object that this annotation represents
+            // Loop  through businesses in the model and find a match
+            for business in map.model.restuarants + map.model.sights {
+                if business.name == view.annotation?.title {
+                    // Set the selectedBusiness property to that business object
+                    map.selectedBusiness = business
+                    return
+                }
+            }
+        }
     }
 }
